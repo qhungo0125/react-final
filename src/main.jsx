4e0d86a84743c470f5c5f07bb260aeea1228efa8
Hungy <hungngojan25@gmail.com>
2023-11-21 16:08:41 +0700

    change router to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './containers/Login/index';
+import Register from './containers/Register/index';
+import Page from './containers/index';
+import NotFound from './components/NotFound';
+import Home from './containers/Home';
+
+const router = createBrowserRouter([
+  { path: '/login', Component: () => <Login /> },
+  { path: '/register', Component: () => <Register /> },
+  { path: '/', Component: () => <Home /> },
+  // { path: '/', Component: () => <Login /> },
+  { path: '/dashboard', Component: () => <Page /> },
+  { path: '*', Component: () => <NotFound /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />;
   </React.StrictMode>,
 );
