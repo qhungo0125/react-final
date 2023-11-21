37b843b9da66548e22e1adfb869eb4313e0096af
Hungy <hungngojan25@gmail.com>
2023-11-21 16:15:08 +0700

    add config to vite

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,12 @@ export default defineConfig({
     port: 3000, // Change this to your desired port
   },
   plugins: [react()],
+  rollupOptions: {
+    external: ['react', 'react-router', 'react-router-dom'],
+    output: {
+      globals: {
+        react: 'React',
+      },
+    },
+  },
 });
