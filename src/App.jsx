e9f927af9003a147bbf05a188796406eb53ba45f
Hungy <hungngojan25@gmail.com>
2023-11-25 19:53:17 +0700

    add global context

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Register from './containers/Register';
+import Home from './containers/Home';
+import Page from './containers';
+import NotFound from './components/NotFound';
+import Login from './containers/Login/index';
+import GlobalContext from './context';
 
-
+const router = createBrowserRouter([
+  { path: '/login', Component: () => <Login /> },
+  { path: '/register', Component: () => <Register /> },
+  { path: '/', Component: () => <Home /> },
+  { path: '/dashboard', Component: () => <Page /> },
+  { path: '*', Component: () => <NotFound /> },
+]);
 
 export default function App() {
+  return (
+    <GlobalContext>
+      <RouterProvider router={router} />;
+    </GlobalContext>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from './containers/Login/index';
-import Register from './containers/Register/index';
-import Page from './containers/index';
-import NotFound from './components/NotFound';
-import Home from './containers/Home';
-
-const router = createBrowserRouter([
-  { path: '/login', Component: () => <Login /> },
-  { path: '/register', Component: () => <Register /> },
-  { path: '/', Component: () => <Home /> },
-  // { path: '/', Component: () => <Login /> },
-  { path: '/dashboard', Component: () => <Page /> },
-  { path: '*', Component: () => <NotFound /> },
-]);
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />;
+    <App />
   </React.StrictMode>,
 );
