34e2f8d931be42a82a95d9092063f9acc16da416
Bao Huy <pbhuy0909@gmail.com>
2023-12-06 14:33:44 +0700

    create class

--- src/context/MenuContext.jsx
+++ src/context/MenuContext.jsx
@@ -1,41 +1,37 @@
-import { useState, createContext } from "react";
+import { useState, createContext } from 'react';
 
 const MenuContext = createContext({
-    tab: "home",
-    classTab: "stream",
-    displayClassTab: false,
-    handleTabChanges: () => {},
-    handleClassTabChanges: () => {},
+  tab: 'home',
+  classTab: 'stream',
+  displayClassTab: false,
+  handleTabChanges: () => {},
+  handleClassTabChanges: () => {},
 });
 
 function MenuProvider({ children }) {
-    const [tab, setTab] = useState("home")
-    const [classTab, setClassTab] = useState("stream");
-    const [displayClassTab, setDisplayClassTab] = useState(false)
+  const [tab, setTab] = useState('home');
+  const [classTab, setClassTab] = useState('stream');
+  const [displayClassTab, setDisplayClassTab] = useState(false);
 
-    const handleTabChanges = (value) => {
-        setDisplayClassTab(false)
-        setTab(value)
-    }
+  const handleTabChanges = (value) => {
+    setDisplayClassTab(false);
+    setTab(value);
+  };
 
-    const handleClassTabChanges = (value) => {
-        setDisplayClassTab(true)
-        setClassTab(value)
-    }
+  const handleClassTabChanges = (value) => {
+    setDisplayClassTab(true);
+    setClassTab(value);
+  };
 
-    const value = {
-        tab,
-        classTab,
-        displayClassTab,
-        handleTabChanges,
-        handleClassTabChanges,
-    }
+  const value = {
+    tab,
+    classTab,
+    displayClassTab,
+    handleTabChanges,
+    handleClassTabChanges,
+  };
 
-    return (
-        <MenuContext.Provider value={value}>
-            {children}
-        </MenuContext.Provider>
-    );
+  return <MenuContext.Provider value={value}>{children}</MenuContext.Provider>;
 }
 
 export { MenuContext, MenuProvider };
