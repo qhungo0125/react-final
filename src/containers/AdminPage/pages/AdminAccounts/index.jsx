3a64dec573410f9739e42f22302cbdaf7a15405f
Hungy <hungngojan25@gmail.com>
2023-12-24 15:09:50 +0700

    center col item

--- src/containers/AdminPage/pages/AdminAccounts/index.jsx
+++ src/containers/AdminPage/pages/AdminAccounts/index.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 const AdminAccounts = () => {
   return (
+    <div className='table-responsive'>
+
     <table className="table table-striped">
       <thead>
-        <tr>
-          <th scope="col">Index</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-          <th scope="col">Actions</th>
+        <tr >
+          <th >Index</th>
+          <th >First</th>
+          <th >Last</th>
+          <th >Handle</th>
+          <th >Actions</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
+        <tr className='align-items-center' style={{verticalAlign: "middle"}}>
           <th scope="row">1</th>
           <td>Mark</td>
           <td>Otto</td>
@@ -34,12 +36,26 @@ const AdminAccounts = () => {
             </div>
           </td>
         </tr>
-        <tr>
+        <tr style={{verticalAlign: "middle"}}>
           <th scope="row">2</th>
           <td>Jacob</td>
           <td>Thornton</td>
           <td>@fat</td>
-          <td>@fat</td>
+          <td>
+            <div
+              class="btn-group"
+              role="group"
+              aria-label="Basic mixed styles example"
+            >
+              <button type="button" class="btn btn-warning">
+                Mapping
+              </button>
+
+              <button type="button" class="btn btn-danger">
+                Block
+              </button>
+            </div>
+          </td>
         </tr>
         <tr>
           <th scope="row">3</th>
@@ -50,6 +66,8 @@ const AdminAccounts = () => {
         </tr>
       </tbody>
     </table>
+    </div>
+
   );
 };
 
