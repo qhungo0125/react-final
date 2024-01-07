eb02e5c9ccf0fbbf90472b095d983f4b0517f5c1
Hung Ngo <hungngojan25@gmail.com>
2024-01-07 17:29:55 +0700

    add publish score feature

--- src/containers/Grade/GradeStudents/GradeStudents.jsx
+++ src/containers/Grade/GradeStudents/GradeStudents.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { getClass, mappingStudent } from '../../../api/admin';
-import { getClassScoreTypes } from '../../../api/scoreStructure';
+import { getClassScoreTypes, updateType } from '../../../api/scoreStructure';
 import { getClassScores, setStudentScore } from '../../../api/scoreDetail';
 import { useSearchParams } from 'react-router-dom';
 import ScoreBoard from './ScoreBoard';
@@ -8,6 +8,7 @@ import EditScore from './EditScore';
 import MappingForm from '../../../components/AdminTable/Accounts/mappingForm';
 import { downloadExcel } from '../../../utils/excel';
 import { t } from 'i18next';
+import PublishForm from './PublishForm';
 
 const GradeStudents = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -16,46 +17,45 @@ const GradeStudents = () => {
   const [selectedStudent, setSelectedStudent] = React.useState({});
   const [openEditForm, setOpenEditForm] = React.useState(false);
   const [openMapForm, setOpenMapForm] = React.useState(false);
+  const [openPublishForm, setOpenPublishForm] = React.useState(false);
+
+  const getClassData = async () => {
+    const classId = searchParams.get('id');
+    try {
+      let [rawStudents, scoresType, scores] = await Promise.all([
+        getClass({
+          id: classId,
+          fields: ['_id', 'email', 'name', 'mapCode'],
+        }),
+        getClassScoreTypes({ classId }),
+        getClassScores({ classId }),
+      ]);
+
+      setScoreTypes(scoresType.data);
+      const studentWithScoreType = rawStudents.data.students.map((student) => {
+        return {
+          ...student,
+          scoreTypes: scoresType.data.map((item) => {
+            const value =
+              scores.data.find(
+                (score) =>
+                  score.student._id === student._id &&
+                  score.type.name === item.name,
+              )?.value || 0;
+            return {
+              ...item,
+              value,
+            };
+          }),
+        };
+      });
+      setStudents(studentWithScoreType);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   React.useEffect(() => {
-    const getClassData = async () => {
-      const classId = searchParams.get('id');
-      try {
-        let [rawStudents, scoresType, scores] = await Promise.all([
-          getClass({
-            id: classId,
-            fields: ['_id', 'email', 'name', 'mapCode'],
-          }),
-          getClassScoreTypes({ classId }),
-          getClassScores({ classId }),
-        ]);
-
-        setScoreTypes(scoresType.data);
-        const studentWithScoreType = rawStudents.data.students.map(
-          (student) => {
-            return {
-              ...student,
-              scoreTypes: scoresType.data.map((item) => {
-                const value =
-                  scores.data.find(
-                    (score) =>
-                      score.student._id === student._id &&
-                      score.type.name === item.name,
-                  )?.value || 0;
-                return {
-                  ...item,
-                  value,
-                };
-              }),
-            };
-          },
-        );
-        setStudents(studentWithScoreType);
-      } catch (error) {
-        console.error(error);
-      } finally {
-      }
-    };
     getClassData();
   }, [selectedStudent]);
 
@@ -117,10 +117,31 @@ const GradeStudents = () => {
     setSelectedStudent({});
   };
 
+  const handlePublishSubmit = async (params) => {
+    const { typeId } = params;
+    try {
+      const response = await updateType({ typeId, isPublish: true });
+      alert('Publish score successfully');
+      setOpenPublishForm(false);
+    } catch (err) {
+      alert('Publish score failed');
+      console.error(err);
+    } finally {
+      getClassData();
+    }
+    return;
+  };
+
   return (
     <>
-      <div className={openEditForm || openMapForm ? 'w-75 opacity-25' : 'w-75'}>
-        <div>
+      <div
+        className={
+          openEditForm || openMapForm || openPublishForm
+            ? 'w-75 opacity-25'
+            : 'w-75'
+        }
+      >
+        <div className='d-flex gap-4'>
           <button
             className='btn btn-success'
             onClick={(e) => {
@@ -135,12 +156,19 @@ const GradeStudents = () => {
                 });
 
                 return extractedStudent;
-                // return student;
               });
               downloadExcel(data);
             }}
           >
             {t('label.export.excel')}
+          </button>
+          <button
+            onClick={(e) => {
+              setOpenPublishForm(true);
+            }}
+            className='btn btn-success'
+          >
+            {t('label.score.publish')}
           </button>
         </div>
         <ScoreBoard
@@ -164,6 +192,13 @@ const GradeStudents = () => {
           isOpen={true}
           setIsOpen={setOpenMapForm}
           onMapping={handleMapStudent}
+        />
+      )}
+      {openPublishForm && (
+        <PublishForm
+          onClose={(e) => setOpenPublishForm(false)}
+          onSubmit={handlePublishSubmit}
+          scoreTypes={scoreTypes}
         />
       )}
     </>
